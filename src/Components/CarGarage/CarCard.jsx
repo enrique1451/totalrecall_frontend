7c724c102e6ec683f1b0939b1859b0faf0566e85
Enrique Hoyos <enrique145@yahoo.com>
2022-05-11 21:27:24 -0400

    Its Alive, finally!

--- src/Components/CarGarage/CarCard.jsx
+++ src/Components/CarGarage/CarCard.jsx
@@ -1,47 +1,54 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 
 import {CardTitle, CardText, Card,  CardImg, CardBody, CardSubtitle, Button} from "reactstrap"
 
 
 
-function CarCard({car}) {
-  const [carsRecallData, setCarsRecallData] = useState({ data:[] })
+function CarCard({car}, {photo}) {
+  const [carsRecallData, setCarsRecallData] = useState([])
   const [isLoading, setIsLoading] = useState(false);
   
   const handleClick = async () => {
-    setIsLoading(true);
+      setIsLoading(true);
+      try {
 
-    try {
-
-      const unproxiedURL = "http://localhost:3001/"
-      const backendRoute = "recalls/recallsByVehicle/"
-
-      const {data} = await axios.get(
-
-        `${unproxiedURL}${backendRoute}`,
+        const unproxiedURL = "http://localhost:3001/"
+        const backendRoute = "recalls/recallsByVehicle/"
+        const recallData = await axios.get(
+          `${unproxiedURL}${backendRoute}`,
+          {
+            "params":{
+              "modelYear":car.yearmodel,
+              "make": car.carmake,
+              "model": car.carmodel
+            }
+          });
         
-        {
-          "params":{
-            "modelYear":car.yearmodel,
-            "make": car.carmake,
-            "model": car.carmodel
-          }
-          
-        },
-      );
-      setCarsRecallData(data);
-
+        setCarsRecallData(recallData.data.results)
+      
       } finally {
-      setIsLoading(false)
-      }
-    };
+        setIsLoading(false);
+      };
+      
+  };
+
+  
 
 
+  
 
-  return (
+ 
+
+    // let id=0; 
+    // const recalls = carsRecallData.data.map(async recall => {
+    //   <CardText recall = {recall} key={id}></CardText>
+
+    // })
+return (
         <Card key={car.car_id}>
-          <CardImg alt="Card image cap" src="https://picsum.photos/318/180" top width="100%" />
+          <CardImg alt="Card image cap" src={photo} width="50%" />          
+         
           <CardBody>
             <CardTitle tag="h5">
               {car.yearmodel}
@@ -49,9 +56,24 @@ function CarCard({car}) {
             <CardSubtitle className="mb-2 text-muted" tag="h6">
               { car.carmake } { car.carmodel }
             </CardSubtitle>
-            <CardText>
-              {car.recalls}
-            </CardText>
+
+            {isLoading &&  <CardText>Loading.... </CardText>}
+            {carsRecallData.length > 0 && (
+             
+                carsRecallData.map(recall => {
+                  return(
+                  <CardText> 
+                    {recall.Component}
+                    {recall.Summary}
+                    {recall.Remedy}
+                  </CardText>
+                  );
+                  })
+              
+            )}
+                         
+
+              
             <Button onClick={handleClick}>
               Get Car Recalls
             </Button>
